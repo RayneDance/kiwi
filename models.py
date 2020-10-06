b8b6e7f5b6882ad3f6788c66a4f985881df9fe53
RayneDance <jordanlalexander@gmail.com>
2020-10-06 04:01:28 +0000

    Database design

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 class Recipes(models.Model):
  
@@ -9,13 +10,17 @@ class Recipes(models.Model):
 	volume = models.FloatField()
 	outPutQuantity = models.IntegerField()
 	time = models.IntegerField()
-	byproducts =  models.ManyToManyField(Recipes)
-	input = models.ManyToManyField('Input', through='Input')
-	
-
+	byproducts =  models.ManyToManyField('Recipes')
+	input = models.ManyToManyField('Recipes', through='Input', related_name="Required")
 
 class Input(models.Model):
-	
-	input = models.ForeignKey('Recipes')
+
+	required = models.ManyToManyField('Recipes', related_name="Subitem")
 	amount = models.FloatField()
 
+class Ore(models.Model):
+	ore_name = models.ForeignKey('Recipes', on_delete=models.SET_NULL, null=True)
+	ore_price = models.FloatField()
+
+admin.site.register(Recipes)
+admin.site.register(Ore)
